7ae04171ea9143ebf08be6e63e35d746b54de1cd
Özgür Toprak Önsoy <50841996+HephaestusHS@users.noreply.github.com>
2022-09-12 21:41:41 +0300

    Add files via upload

---
+++ IInventoryItem.cs
@@ -0,0 +1,17 @@
+﻿namespace HephUnity.Inventory
+{
+    /// <summary>
+    /// Provides properties to help storing items in an inventory.
+    /// </summary>
+    public interface IInventoryItem
+    {
+        /// <summary>
+        /// Specifies the number of rows the item occupies when stored in an <seealso cref="Inventory"/>.
+        /// </summary>
+        public uint RowSize { get; set; }
+        /// <summary>
+        /// Specifies the number of columns the item occupies when stored in an <seealso cref="Inventory"/>.
+        /// </summary>
+        public uint ColSize { get; set; }
+    }
+}
---
+++ MaxStackCountAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace HephUnity.Inventory
+{
+    /// <summary>
+    /// Specifies the maximum number of elements that can be stacked in an <seealso cref="IInventoryItem"/>.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = true)]
+    public class MaxStackCountAttribute : Attribute
+    {
+        public int MaxStackCount { get; }
+        public MaxStackCountAttribute(int maxStackCount) => MaxStackCount = maxStackCount;
+    }
+}
